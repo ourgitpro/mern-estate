174df3fe7f9610efe091a79f87593fcb59c95b16
Md. Nahid Hossain Eblo <56239953+ourgitpro@users.noreply.github.com>
2024-01-11 10:16:10 -0800

    new app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
+////mongoose.connect('mongodb+srv://user:user123456@cluster0.ld9e4.mongodb.net/WWE?retryWrites=true&w=majority',()=>{
+// console.log("DB Connected")
