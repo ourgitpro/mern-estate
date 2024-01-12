36698aa9ba4002543655c170023e84ed585f0798
Md. Nahid Hossain Eblo <56239953+ourgitpro@users.noreply.github.com>
2024-01-11 22:30:19 -0800

    sign up work

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,26 +1,41 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+  }
+  console.log(formData)
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           id="username"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           id="email"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           id="password"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           Sign Up
